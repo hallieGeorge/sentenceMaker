d12cf9520be0d866d57f2f9e7929f54c01e27b81
Hallie George <hallie.george1204@gmail.com>
2026-01-20 23:33:55 -0500

    sentenceMaker.js

---
+++ sentenceMaker.js
@@ -0,0 +1,19 @@
+let adjective = "small";
+let noun = "Elora";
+let verb = "cold";
+let place = "park";
+let adjective2 = "happy";
+let noun2 = "Cheerio's"
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "short";
+noun = "Phoenix";
+verb = "hot";
+place = "dumpster";
+adjective2 = "large";
+noun2 = "steak";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
